872af4cdea9dd42431112af8fe12c07cc3daf691
Tyler Hancock <darklime@live.ca>
2017-09-11 17:30:54 -0600

    Created a hash generator for Java's message digest.

---
+++ src/main/java/com/diluv/identicon/hashgen/HashGeneratorDigest.java
@@ -0,0 +1,52 @@
+package com.diluv.identicon.hashgen;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * This hash generator uses Java's MessageDigest system. Each implementation of the JRE has
+ * different generators available. The JRE specifications require every implementation to
+ * support MD5, SHA-1, and SHA-256.
+ *
+ * @author Tyler Hancock (Darkhax)
+ */
+public class HashGeneratorDigest implements IHashGenerator {
+
+    /**
+     * The method digest algorithm that is used by the generator.
+     */
+    private MessageDigest messageDigest;
+
+    /**
+     * Constructs a hash generator that uses Java's MessageDigest system. This can be used to
+     * generate things like MD5 hashes. If you use an invalid digest algorithm, the generator
+     * will not work.
+     *
+     * @param algorithim The algorithm to use. Each JRE can have different generators
+     *        available. The JRE specifications require every implementation to support MD5,
+     *        SHA-1, and SHA-256.
+     */
+    public HashGeneratorDigest (String algorithim) {
+
+        try {
+
+            this.messageDigest = MessageDigest.getInstance(algorithim);
+        }
+
+        catch (final NoSuchAlgorithmException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public byte[] getHash (String input) {
+
+        return this.messageDigest.digest(input.getBytes());
+    }
+
+    public String getAlgorithmName () {
+
+        return this.messageDigest.getAlgorithm();
+    }
+}
