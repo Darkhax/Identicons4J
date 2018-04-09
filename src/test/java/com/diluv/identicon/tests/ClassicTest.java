2e472831dc1d95cc239f2bbe517812aa02cd803c
Tyler Hancock <darklime@live.ca>
2018-04-08 21:00:59 -0600

    Moved to JUnit for proper testing.

--- src/test/java/com/diluv/identicon/tests/ClassicTest.java
+++ src/test/java/com/diluv/identicon/tests/ClassicTest.java
@@ -1,48 +1,25 @@
 package com.diluv.identicon.tests;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
-import javax.imageio.ImageIO;
+import org.junit.Assert;
+import org.junit.Test;
 
-import com.diluv.identicon.Identicons;
-import com.diluv.identicon.generators.ClassicGenerator;
-import com.diluv.identicon.generators.IIdenticonGenerator;
 import com.diluv.identicon.hashgen.HashGeneratorDigest;
 
-/**
- * This test generates some identicons for md5, sha1 and sha256 digests.
- * 
- * @author Tyler Hancock (Darkhax)
- */
 public class ClassicTest {
-    
-    public static void main (String[] args) {
-        
-        final IIdenticonGenerator identiconGenerator = new ClassicGenerator();
-        final HashGeneratorDigest[] hashGenerators = { new HashGeneratorDigest("MD5"), new HashGeneratorDigest("SHA-1"), new HashGeneratorDigest("SHA-256") };
-        
-        for (final HashGeneratorDigest hashGen : hashGenerators) {
-            
-            final BufferedImage image = Identicons.scaleUpImage(identiconGenerator.generate("Classic-Test", hashGen), 256, 256);
-            
-            try {
-                
-                final File file = new File("test/classic/" + hashGen.getAlgorithmName() + ".png");
-                
-                if (!file.getParentFile().exists()) {
-                    
-                    file.mkdirs();
-                }
-                
-                ImageIO.write(image, "PNG", file);
-            }
-            
-            catch (final IOException e) {
-                
-                e.printStackTrace();
-            }
-        }
+
+    @Test
+    public void testDigestGenerators () throws NoSuchAlgorithmException {
+
+        this.testDigest("MD5", new byte[] { 9, -113, 107, -51, 70, 33, -45, 115, -54, -34, 78, -125, 38, 39, -76, -10 });
+        this.testDigest("SHA-1", new byte[] { -87, 74, -113, -27, -52, -79, -101, -90, 28, 76, 8, 115, -45, -111, -23, -121, -104, 47, -69, -45 });
+        this.testDigest("SHA-256", new byte[] { -97, -122, -48, -127, -120, 76, 125, 101, -102, 47, -22, -96, -59, 90, -48, 21, -93, -65, 79, 27, 43, 11, -126, 44, -47, 93, 108, 21, -80, -16, 10, 8 });
+    }
+
+    private void testDigest (String algorithm, byte[] knownGood) throws NoSuchAlgorithmException {
+
+        final HashGeneratorDigest gen = new HashGeneratorDigest(algorithm);
+        Assert.assertArrayEquals(gen.getHash("test"), knownGood);
     }
 }
